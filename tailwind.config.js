6cda936419f2759ab7d0c52b186c693258feb92e
Jeason Campos <camposjeason@gmail.com>
2022-08-22 11:53:10 -0400

    nextJs + Tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */ 
+module.exports = {
+  content: [
+    "../prueba/src/pages/**/*.{js,jsx,ts,tsx}",
+    "../prueba/src/Components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
